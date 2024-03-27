7b1ef31cf9c06cfd5c4cb1b850fbcaf83d0eb31d
Ahlam-Banu <ahlambanu.univ@gmail.com>
2024-03-27 14:13:48 +0200

    updated

--- SOAPservice.py
+++ SOAPservice.py
@@ -7,12 +7,9 @@ wsdl_url = 'https://www.crcind.com/csp/samples/SOAP.Demo.cls?WSDL'
 client = Client(wsdl_url)
 
 # Call the functions provided by the SOAP service
-result1 = client.service.AddInteger(5, 10)  # Assuming AddInteger takes two integers as parameters
-result2 = client.service.DivideInteger(10, 2)  # Assuming DivideInteger takes two integers as parameters
-result3 = client.service.FindPerson("John Doe")  # Assuming FindPerson takes a string parameter
+result1 = client.service.AddInteger(5, 10)
+result2 = client.service.DivideInteger(10, 2)
 
 # Process the responses
 print("Result of AddInteger:", result1)
 print("Result of DivideInteger:", result2)
-print("Result of FindPerson:", result3)
-# Process other responses as needed...
