5f4d3308c4e5cce05485ac0093f5212873f9b6f1
Ahlam-Banu <ahlambanu.univ@gmail.com>
2024-03-27 11:14:50 +0200

    SOAP service script with method calls

--- SOAPservice.py
+++ SOAPservice.py
@@ -1,4 +1,4 @@
-from suds.client import Client
+from zeep import Client
 
 # Define the WSDL URL of the SOAP service
 wsdl_url = 'https://www.crcind.com/csp/samples/SOAP.Demo.cls?WSDL'
@@ -7,11 +7,12 @@ wsdl_url = 'https://www.crcind.com/csp/samples/SOAP.Demo.cls?WSDL'
 client = Client(wsdl_url)
 
 # Call the functions provided by the SOAP service
-result1 = client.service.Method1()
-result2 = client.service.Method2()
-# Call other methods as needed...
+result1 = client.service.AddInteger(5, 10)  # Assuming AddInteger takes two integers as parameters
+result2 = client.service.DivideInteger(10, 2)  # Assuming DivideInteger takes two integers as parameters
+result3 = client.service.FindPerson("John Doe")  # Assuming FindPerson takes a string parameter
 
 # Process the responses
-print("Result of Method1:", result1)
-print("Result of Method2:", result2)
+print("Result of AddInteger:", result1)
+print("Result of DivideInteger:", result2)
+print("Result of FindPerson:", result3)
 # Process other responses as needed...
