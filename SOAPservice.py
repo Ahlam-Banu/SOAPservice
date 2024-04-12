4f95fddf028679a4ea211946242a004e73913854
Ahlam Fazlu Rahman <102663986+Ahlam-Banu@users.noreply.github.com>
2024-04-12 17:44:09 +0300

    git.dev testing

--- SOAPservice.py
+++ SOAPservice.py
@@ -6,6 +6,7 @@ wsdl_url = 'https://www.crcind.com/csp/samples/SOAP.Demo.cls?WSDL'
 # Create a SOAP client
 client = Client(wsdl_url)
 
+#this is done from github.dev online
 # Call the functions provided by the SOAP service
 result1 = client.service.AddInteger(5, 10)
 result2 = client.service.DivideInteger(10, 2)
